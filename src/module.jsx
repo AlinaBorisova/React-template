5837a74f08217639bdea08fbb18a77592b297976
Алина Борисова <b-alina1999.borisova@yandex.ru>
2023-09-25 18:48:22 +0300

    init

---
+++ src/module.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import pic from './img/imgTest.png'
+
+const Module = () => (
+    <>
+        <h1>Hello, React!</h1>
+        <img width={200} src={pic}/>
+    </>
+);
+
+export default Module;
